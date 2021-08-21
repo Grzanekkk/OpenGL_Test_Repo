36b97c929f2c06747184c7301a281bc53325d20c
Jan Puto <tofukles@gmail.com>
2021-08-21 15:47:46 +0200

    Vertex layout

---
+++ OpenGL_Test/src/VertexArray.cpp
@@ -0,0 +1,39 @@
+#include "VertexArray.h"
+#include "Renderer.h"
+
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_RendererID));
+}
+
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_RendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout)
+{
+	Bind();
+	vb.Bind();
+	const std::vector<VertexBufferElement>& elements = layout.GetElements();
+	unsigned int offset = 0;
+
+	for (unsigned int index = 0; index < elements.size(); index++)
+	{
+		const VertexBufferElement element = elements[index];
+		GLCall(glEnableVertexAttribArray(index));
+		GLCall(glVertexAttribPointer(index, element.count, element.type, element.normailzed, layout.GetStride(), (const void*)offset));  // Links vertex buffer with vao
+		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+	}
+
+}
+
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_RendererID));
+}
+
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
+}
--- OpenGL_Test/src/VertexArray.h
+++ OpenGL_Test/src/VertexArray.h
@@ -6,10 +6,13 @@
 class VertexArray
 {
 private:
-
+	unsigned int m_RendererID;
 public:
 	VertexArray();
 	~VertexArray();
 
-	void AddBuffer(const VertexBuffer& cv, const VertexBufferLayout& layout);
+	void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+	void Bind() const;
+	void Unbind() const;
 };
