f26d37ecb9e1a15b0ce41884fa2cf7e3f026f122
Jan Puto <tofukles@gmail.com>
2021-08-30 23:31:33 +0200

    Code cleanup and bases for test classes

--- OpenGL_Test/src/Application.cpp
+++ OpenGL_Test/src/Application.cpp
@@ -21,6 +21,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 
 int main(void)
 {
@@ -48,46 +50,8 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[] = {
-         -50.0f, -50.0f, 0.0f, 0.0f, // 0
-          50.0f, -50.0f, 1.0f, 0.0f, // 1
-          50.0f,  50.0f, 1.0f, 1.0f, // 2
-         -50.0f,  50.0f, 0.0f, 1.0f  // 3
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));  // Allows blending/transparency
-
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-    VertexBufferLayout layout;
-    layout.Push<float>(2);  // vertices coords
-    layout.Push<float>(2);  // texture coords
-    va.AddBuffer(vb, layout);
-
-    IndexBuffer ib(indices, 6);
-
-    glm::mat4 projection = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);   // Converts 3D world coordinates to screen space coordinates between -1 and 1
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-    Shader shader("res/shaders/Basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.5f, 0.8f, 1.0f);
-
-    Texture texture("res/textures/papaj.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);
-    
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
     
     Renderer renderer;
 
@@ -96,62 +60,28 @@ int main(void)
     ImGui_ImplOpenGL3_Init();
     ImGui::StyleColorsDark();
 
-    float r = 0.0f;
-    float increment = 0.01f;
-
-    glm::vec3 translationA(200, 200, 0);
-    glm::vec3 translationB(400, 400, 0);
+    test::TestClearColor test;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         renderer.Clear();
 
+        test.OnUpdate(0.0f);
+        test.OnRender();
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-        shader.Bind();
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            glm::mat4 mvp = projection * view * model;
-            shader.SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(va, ib, shader);
-        }
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            glm::mat4 mvp = projection * view * model;
-            shader.SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(va, ib, shader);
-        }
-
-        if (r >= 1.0f)
-            increment = -0.01f;
-        else if (r <= 0.0f)
-            increment = 0.01f;
-
-        r += increment;
-
-		{
-			ImGui::Begin("Cool debug window!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-            ImGui::End();
-		}
+        test.OnImGuiRender();
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
