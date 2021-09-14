0b941a18235fd9d26a82c9aa85946e60c4ebfc66
Jan Puto <tofukles@gmail.com>
2021-09-14 14:33:15 +0200

    I have no idea how to fix it

--- OpenGL_Test/src/VertexBuffer.cpp
+++ OpenGL_Test/src/VertexBuffer.cpp
@@ -13,12 +13,12 @@ VertexBuffer::VertexBuffer(const void* data, unsigned int size)     // Data == p
 VertexBuffer::VertexBuffer(float vertexData[], float textureCoordData[], unsigned int size)     // Data == positions, rotations etc. of Vertex
 	: m_Size(size)
 {
-	std::vector<float>m_VertexData(vertexData, vertexData + sizeof(vertexData) / sizeof(vertexData[0]));
+	std::vector<float> m_VertexData(vertexData, vertexData + sizeof(vertexData) / sizeof(vertexData[0]));
 	std::vector<float> m_TextureCoordData(textureCoordData, textureCoordData + sizeof(textureCoordData) / sizeof(textureCoordData[0]));
-	const void* data = CombineVertexWithTextureCoord(vertexData, textureCoordData);
+	
 	GLCall(glGenBuffers(1, &m_RendererID));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, m_Size, data, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, m_Size, CombineVertexWithTextureCoord(vertexData, textureCoordData), GL_STATIC_DRAW));
 }
 
 VertexBuffer::~VertexBuffer()
@@ -36,27 +36,29 @@ void VertexBuffer::Unbind() const
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 }
 
-void* VertexBuffer::CombineVertexWithTextureCoord(float vertexData[], float textureCoordData[]) const
+void* VertexBuffer::CombineVertexWithTextureCoord(float vertexData[], float textureCoordData[]) const 
 {
-	std::vector<float> data;
+	float data[56];				// Magic Number :flushed:
+	int dataIndex = 0;
 	int vertexID = 0;
-	int textureID = 0 ;
+	int textureID = 0;
 
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			data.push_back(vertexData[vertexID]);
+			data[dataIndex] = vertexData[vertexID];
 			vertexID++;
+			dataIndex++;
 		}
-		for (int k = 0; k < 4; k++)
+		for (int k = 0; k < 2; k++)
 		{
-			data.push_back(textureCoordData[textureID]);
+			data[dataIndex] = textureCoordData[textureID];
 			textureID++;
+			dataIndex++;
 		}
 	}
 
 
-	void* ptr = std::data(data);
-	return ptr;
+	return (void*)data;
 }
--- OpenGL_Test/src/examples/Example3DCube.cpp
+++ OpenGL_Test/src/examples/Example3DCube.cpp
@@ -25,18 +25,26 @@ namespace example
 			-1.0f, -1.0f, -1.0f,
 			 1.0f, -1.0f, -1.0f,
 			 1.0f,  1.0f, -1.0f,
-			-1.0f,  1.0f, -1.0f	// back face
+			-1.0f,  1.0f, -1.0f		// back face
 		};
 
 		float textureUV[] = {
-			0.0f, 0.0f, 1.0f, 0.0f,
-			1.0f, 1.0f, 0.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 0.0f,
-			1.0f, 1.0f, 0.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 0.0f,
-			1.0f, 1.0f, 0.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 0.0f,
-			1.0f, 1.0f, 0.0f, 1.0f
+			0.0f, 0.0f,
+			1.0f, 0.0f,
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			0.0f, 0.0f,
+			1.0f, 0.0f,
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			0.0f, 0.0f,
+			1.0f, 0.0f,
+			1.0f, 1.0f,
+			0.0f, 1.0f,
+			0.0f, 0.0f,
+			1.0f, 0.0f,
+			1.0f, 1.0f,
+			0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -51,11 +59,11 @@ namespace example
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-		m_VertexBuffer = std::make_unique<VertexBuffer>(vertices, textureUV, (8 * 3 * 4) * sizeof(float));
+		m_VertexBuffer = std::make_unique<VertexBuffer>(vertices, textureUV, (32 + 24) * sizeof(float));
 
 		VertexBufferLayout layout;
-		layout.Push<float>(5);
-		//layout.Push<float>(2);
+		layout.Push<float>(3);
+		layout.Push<float>(2);
 
 		m_Texture = std::make_unique<Texture>("res/textures/spongebob.png");
 		//m_Texture->BindTextureUV(textureUV, 4 * 8 * sizeof(float));
